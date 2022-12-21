c076627f1a563b8fbe24620906ca8e6b9944b86a
pablomoga9 <moyagarcia99@gmail.com>
2022-12-21 14:14:13 +0100

    convert/xml to json

--- src/components/Main/Podcast/Podcast.jsx
+++ src/components/Main/Podcast/Podcast.jsx
@@ -1,25 +1,39 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+ 
+
 const Podcast = () => {
+  var XMLParser = require('react-xml-parser');
   const [podcastData, setPodcastData] = useState(null);
   useEffect(() => {
     const getPodcast = async () => {
       try {
         let getId = (window.location.pathname).split("podcast/")[1];
         const res = await axios.get(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://itunes.apple.com/lookup?id=${getId}`)}`)
-          const jsonRes = JSON.parse(res.data.contents)
-           setPodcastData (jsonRes)
+        const jsonRes = JSON.parse(res.data.contents)
+        setPodcastData (jsonRes)
+           
+        console.log(jsonRes.results[0].feedUrl);
+        const episodes = await axios.get(`https://api.allorigins.win/get?url=${encodeURIComponent(`${jsonRes.results[0].feedUrl}`)}`);
+        console.log(episodes.data.contents)
+        const xmlJson = new XMLParser().parseFromString(episodes.data.contents)
+        console.log(xmlJson)
+           
       }
       catch (error) {
         console.log(error);
       }
     }
     getPodcast();
-  },[setPodcastData])
+  },[])
+
+ 
+
+
   return <>
     <div className="podcastInfo">
-     {podcastData !== null ? <img src={podcastData.results[0].artworkUrl100}></img>:<h2>no hay</h2>}
+     {podcastData !== null ? <img src={podcastData.results[0].artworkUrl100} alt=""></img>:<h2>Loading...</h2>}
     </div>
   </>;
 };
