b0a968f7aba9407f1e60aec04f89b962201007c0
pablomoga9 <moyagarcia99@gmail.com>
2022-12-29 19:49:49 +0100

    fix

--- src/components/Main/Podcast/Episode/Episode.jsx
+++ src/components/Main/Podcast/Episode/Episode.jsx
@@ -1,24 +1,8 @@
-import React, { useEffect, useState } from "react";
-import Play from "../../Play/Play";
-import { Link } from "react-router-dom";
+import React from "react";
+
 const Episode = (props) => {
-  const [id,setId] = useState(null);
-  console.log(props.data)
 
 
-  useEffect(()=>{
-    const getId = async()=>{
-      try{
-        const podcastId = (window.location.pathname).split("podcast/")[1];
-        await setId(podcastId);
-        console.log(podcastId);
-      }
-      catch(error){
-        console.log(error);
-      }
-    }
-    getId();
-  },[])
 
   function getTitle(arr){
     const title = arr.filter(item=>{
@@ -40,21 +24,13 @@ const Episode = (props) => {
     return duration[0].value;
   }
 
-  function getEpisodeId(arr){
-    const episodeId = arr.filter(item=>{
-      return item.name === "guid";
-    })
-    return episodeId[0].value;
-  }
-
   return <div className="episodesList">
     {console.log(props.data)}
     <h3>Episodes:{props.data.length}</h3>
     <ul className="list">
       {props.data.map((item,i)=>{
        return <li key={i}>
-        {console.log(item.children)}
-        <Link to={{pathname:`/podcast/${id}/episode/${getEpisodeId(item.children)}` ,state:{info:"holaholahola"}}}>{getTitle(item.children)}</Link>
+        <a href=""><p>{getTitle(item.children)}</p></a>
         <p>{getDate(item.children)}</p>
         <p>{getDuration(item.children)}</p>
        </li>
